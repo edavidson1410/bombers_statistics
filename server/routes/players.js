7b450bc6ed32fcd2e9253b4ac977c5d0a48a96f8
Eric Davidson <edavidson1410@gmail.com>
2023-10-16 17:38:06 -0500

    POST to mysql database added and tested.

--- server/routes/players.js
+++ server/routes/players.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const Player = require("../models/player")
 const db = require('../configs/dbConfig');
 
 // Getting all players
@@ -16,12 +15,29 @@ router.get('/', (req, res) => {
 
 // Getting one player
 router.get('/:id', (req, res) => {
-    res.send(req.params.id);
+    db.query(`SELECT * FROM players WHERE id=${req.params.id}`, (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    })
 })
 
 // Creating player
 router.post('/', (req, res) => {
-    
+    const name = req.body.name;
+    const position = req.body.position;
+    const appearances = req.body.appearances;
+    const tries = req.body.tries;
+    db.query(`INSERT INTO players VALUES (?, ?, ?, ?, ?)`, [0, name, position, appearances, tries], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send(result);
+        }
+    })
+
 })
 
 // Updating player (patched used to only update given changes vs put would change entire entry)
