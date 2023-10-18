235ce5d81e45732d60ca0f4726f64cc52758ab31
Eric Davidson <edavidson1410@gmail.com>
2023-10-18 18:42:53 -0500

    CRUD processes for players finished

--- server/routes/players.js
+++ server/routes/players.js
@@ -3,36 +3,41 @@ const router = express.Router();
 const db = require('../configs/dbConfig');
 
 // Getting all players
-router.get('/', (req, res) => {
+router.get('/', async(req, res) => {
     db.query('SELECT * FROM players', (err, result) => {
         if (err) {
             console.log(err);
+            res.send(500);
         } else {
+            res.status(200);
             res.send(result);
         }
     })
 })
 
 // Getting one player
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
     db.query(`SELECT * FROM players WHERE id=${req.params.id}`, (err, result) => {
         if (err) {
             console.log(err);
+            res.send(500);
         } else {
+            res.status(200);
             res.send(result);
         }
     })
 })
 
 // Creating player
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
     const name = req.body.name;
     const position = req.body.position;
-    const appearances = req.body.appearances;
+    const caps = req.body.caps;
     const tries = req.body.tries;
-    db.query(`INSERT INTO players VALUES (?, ?, ?, ?, ?)`, [0, name, position, appearances, tries], (err, result) => {
+    db.query(`INSERT INTO players VALUES (?, ?, ?, ?, ?)`, [0, name, position, caps, tries], (err, result) => {
         if (err) {
             console.log(err);
+            res.send(500);
         } else {
             res.send(result);
         }
@@ -41,13 +46,42 @@ router.post('/', (req, res) => {
 })
 
 // Updating player (patched used to only update given changes vs put would change entire entry)
-router.patch('/', (req, res) => {
+router.patch('/:id', async(req, res) => {
+    let id = req.params.id;
+    const name = req.body.name;
+    const position = req.body.position;
+    const caps = req.body.caps;
+    const tries = req.body.tries;
 
+    db.query(`UPDATE players
+    SET name = IFNULL(?, name), position = IFNULL(?, position), caps=IFNULL(?, caps), tries=IFNULL(?, tries) WHERE id = ?`, [
+        name,
+        position,
+        caps,
+        tries,
+        id
+    ], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.send(500);
+        } else {
+            res.send(result);
+        }
+    })
 })
 
 // Deleting player
-router.delete('/', (req, res) => {
+router.delete('/:id', async(req, res) => {
+    const id = req.params.id;
 
+    db.query(`DELETE FROM players WHERE id=?`, [id], (err, result) => {
+        if (err) {
+            console.log(err);
+            res.send(500);
+        } else {
+            res.send(result);
+        }
+    })
 })
 
 module.exports = router;
